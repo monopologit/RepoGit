cd7806f7c1902ede24066bbec46a8e8d49459244
monopologit <monopolar@gmail.com>
2024-04-30 11:11:09 -0300

    DatComplex01

---
+++ datos_complejos.py
@@ -0,0 +1,7 @@
+materias_por_estudiante = {
+    "Ivan": ["Programacion I", "Programacion II", "Ciencia de datos"],
+    "Paola": ["Matematica", "Fisica", "Lengua"],
+    "Anabella": ["Historia", "Literatura", "Biologia"]
+}
+print(materias_por_estudiante)
+print(materias_por_estudiante["Paola"][0:2])
